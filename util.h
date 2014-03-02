228140ac605f033720c3ae228e97cc0e7c5a8897
Squall1931 <vuong033@umn.edu>
2014-03-02 02:45:33 -0600

    Create util.h
    Need to update the inputs to the methods.

---
+++ util.h
@@ -0,0 +1,12 @@
+// The following ifdef/def pair prevents us from having problems if 
+// we've included util.h in multiple places... it's a handy trick
+// Used from Project 1
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+FILE* out_file(char*);
+FILE* log_file(char*);
+char* current_directory();
+char* output_stream();
+
+#endif
